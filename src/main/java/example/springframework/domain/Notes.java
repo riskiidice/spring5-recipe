f747248435616f2f35eb2b9eaacd99608f6183bb
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2018-01-12 23:08:46 +0700

    fix

--- src/main/java/example/springframework/domain/Notes.java
+++ src/main/java/example/springframework/domain/Notes.java
@@ -1,7 +1,10 @@
 package example.springframework.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Notes {
 	@Id
@@ -13,26 +16,6 @@ public class Notes {
 
 	@Lob
 	private String recipeNotes;
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Recipe getRecipe() {
-		return recipe;
-	}
-	public void setRecipe(Recipe recipe) {
-		this.recipe = recipe;
-	}
-	public String getRecipeNotes() {
-		return recipeNotes;
-	}
-	public void setRecipeNotes(String recipeNotes) {
-		this.recipeNotes = recipeNotes;
-	}
-	
-	
+
+
 }
